9be4abfb996e7331e9d88eb09f1b859b16722099
Anastassiya Duisembayeva <imperator-taya@mail.ru>
2019-05-10 23:14:57 +0600

    transfer 2 mapkit to onResume

--- app/src/main/java/com/example/bsfragments/contacts.java
+++ app/src/main/java/com/example/bsfragments/contacts.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,10 +15,7 @@ import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.mapview.MapView;
 
 public class contacts extends Fragment {
-    private MapView mapview;
-    private final String MAPKIT_API_KEY = "7944a636-4992-4f4f-90e2-5cbdde8bf9a9";
-    private final Point TARGET_LOCATION = new Point(43.255312, 76.947213);
-
+    public MapView mapview;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,6 +33,9 @@ public class contacts extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        //перенесенные строки
+        final String MAPKIT_API_KEY = "7944a636-4992-4f4f-90e2-5cbdde8bf9a9";
+        final Point TARGET_LOCATION = new Point(43.255312, 76.947213);
         MapKitFactory.setApiKey(MAPKIT_API_KEY);
         MapKitFactory.initialize(getActivity());
         mapview = (MapView)getView().findViewById(R.id.map);
